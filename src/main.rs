86fc2584935c87bc890be3422b379b491c25ed1a
akakou <akakou571@gmail.com>
2022-04-22 18:36:38 +0900

    [add] permuteを理解するためのテストコード

--- src/main.rs
+++ src/main.rs
@@ -83,3 +83,54 @@ fn main() {
     }
 }
 
+
+
+fn simurate_permute(vec: [f64; 4], cmd: u8) -> [f64; 4] {
+    let cmd1 = cmd & 0b1111;
+    let cmd2 = (cmd & 0b11110000) << 4;
+
+    println!("cmd1: {}", cmd1);
+    println!("cmd2: {}", cmd2);
+    
+    fn part_of_simurate_permute(vec: [f64; 4], cmd: u8, base: usize) -> [f64; 2]{
+        let mut res = [0., 0.];
+
+        let zero = base;
+        let one = base + 1;
+
+        if cmd == 0 {
+            res = [vec[zero], vec[zero]]
+        }
+
+        if cmd == 1 {
+            res = [vec[one], vec[zero]]
+        }
+
+        if cmd == 10 {
+            res = [vec[zero], vec[one]]
+        }
+
+        if cmd == 11 {
+            res = [vec[one], vec[one]]
+        }
+
+        return res;
+    }
+
+    let res1 = part_of_simurate_permute(vec, cmd1, 0);
+    let res2 = part_of_simurate_permute(vec, cmd2, 2);
+    
+        
+    println!("res1: {:?}", res1);
+    println!("res2: {:?}", res2);
+    
+    return [res1[0], res1[1], res2[0], res2[1]];
+}
+
+#[test]
+fn test_simurate_permute() {
+    let vec = [0.,1.,2.,3.];
+
+    let res = simurate_permute(vec, 0b0000);
+    println!("my permute: {:?}, {:b} => {:?}", vec, 0b0000, res);
+}
